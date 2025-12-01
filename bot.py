06c9298741f798d658c8d2e6e2eb7e5c0cfb66f1
DRAWCOFF <pedrohenriquesampaioduartesilv@gmail.com>
2025-12-01 09:40:16 -0300

    correção do token e prefixo

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ if not token:
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 async def load_cogs():
     for filename in sorted(os.listdir("cogs")):
